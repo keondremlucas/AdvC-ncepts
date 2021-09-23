aaf8715d04dad8b4e95ff34501cc24efb8dc0410
Bag Chaser <keondre.m.lucas@my.ccsu.edu>
2021-09-23 12:01:32 -0500

    Started

---
+++ DataPipe.cs
@@ -0,0 +1,26 @@
+using System;
+using System.IO;
+namespace AdvC_ncepts
+{   
+    
+    public class DataPipe
+    {
+        int A;
+        int B;
+        int C;
+        
+        public Data(int a, int b, int c)
+        {
+            A = a;
+            B = b;
+            C = c;
+        }
+        
+
+
+
+
+
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+using System.Collections.Generic;
+
+
+namespace AdvC_ncepts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var data = File.ReadAllText("./data.json");
+            var datajson = JsonSerializer.Deserialize<List<DataPipe>>(data);
+            foreach(var a in datajson)
+            Console.WriteLine(a.data);
+
+        }
+    }
+}
